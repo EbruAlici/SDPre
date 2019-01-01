aefcd7c28224726caa3e368362cd984e8573b592
Ebru Alici <ebrugulnaz@gmail.com>
2019-01-01 13:37:20 -0700

    Objective2Lab1-5

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Ebru Alici";
+    int age = 45;
+    String state = "Colorado";
+
+    name = "Ebru Alici";
+    age = 45;
+    state = "Colorado";
+    System.out.println( "Name: " + name + " " + "Age: " + age + " " + "State: " + state );
+
+  }
+}
