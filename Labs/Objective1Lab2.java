dbb4fadeb16a622cb525faf462b7f51ae76b8216
Ebru Alici <ebrugulnaz@gmail.com>
2018-12-31 17:08:33 -0700

    Practice Objective1Lab1-3

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,7 @@
+public class Objective1Lab2 {
+  public static void main(String[] args ) {
+    System.out.println( "Ebru Alici" );
+    System.out.println( "My age is 45" );
+  }
+
+}
---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main( String[] args ) {
+    System.out.println( "Name : Ebru Alici" );
+    System.out.println( "Age : 45 " );
+    System.out.println( "State : Colorado ");
+  }
+}
