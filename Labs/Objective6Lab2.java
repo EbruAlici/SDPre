492af90c5f7160843bb3a653b6b908ae6b348821
Ebru Alici <ebrugulnaz@gmail.com>
2019-01-05 19:39:03 -0700

    Objective6Lab1-5

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,14 @@
+public class Objective6Lab2 {
+  public static void main(String[] args){
+  int counter = 0;
+
+  while(counter <= 20){
+    if( counter == 0 ){
+      counter = 1;
+    }
+    System.out.println(counter);
+    counter = counter + 1;
+
+  }
+ }
+}
---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,16 @@
+
+
+public class Objective6Lab4 {
+  public static void main(String[] args){
+    int total = 0, count = 1;
+
+
+
+
+    while (count <= 20) {
+      total += count;
+      count += 1;
+    }
+    System.out.println(+total);
+  }
+}
